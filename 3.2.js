dc39844db16a492a516e94dfe8af7a49dba46bed
shweta09111 <shweta09111@gmail.com>
2024-06-05 18:29:43 +0530

    final commit

---
+++ 3.2.js
@@ -0,0 +1,29 @@
+let headings = [
+    'Amazing tool! Saved me months',
+    'Amazing tool! Saved me months 2',
+    'Amazing tool! Saved me months 3',
+    'Amazing tool! Saved me months 4'];
+
+let paragraph = [
+    'This is a placeholder for your testimonials and what your client has to say, put them here and make sure its 100% true and meaningful.',
+    'This is a placeholder for your testimonials and what your client has to say, put them here and make sure its 100% true and meaningful. 2',
+    'This is a placeholder for your testimonials and what your client has to say, put them here and make sure its 100% true and meaningful. 3',
+    'This is a placeholder for your testimonials and what your client has to say, put them here and make sure its 100% true and meaningful. 4'];
+
+let main = document.getElementById("section-3-bottom");
+let col = "";
+for (let i = 0; i<4; i++) {
+    col += `<div class="col">
+    <h3>${headings[i]}</h3>
+    <p>${paragraph[i]}</p>
+    <div class="info">
+        <img src="images/avatar.png" alt="">
+        <div class="info-text">
+            <p>John Master</p>
+            <p>Director,  UiFry.com</p>
+        </div>
+    </div>
+</div>`
+}
+
+main.innerHTML=col
